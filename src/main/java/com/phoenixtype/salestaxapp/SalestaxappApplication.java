b065d049b7da15789e117b3d83948c9bee07c16d
Samuel Akuma <usakuma@uwaterloo.ca>
2023-04-05 11:15:28 -0400

    Initial springboot setup

---
+++ src/main/java/com/phoenixtype/salestaxapp/SalestaxappApplication.java
@@ -0,0 +1,13 @@
+package com.phoenixtype.salestaxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalestaxappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalestaxappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phoenixtype/salestaxapp/SalestaxappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenixtype.salestaxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalestaxappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
